df9faf391d29495852d0838c71aa9e1d79f77d90
Denis <kade01@yandex.ru>
2022-09-18 14:44:42 +0300

    2 commit

--- DZ_1/Program.cs
+++ DZ_1/Program.cs
@@ -7,3 +7,10 @@ string userEnter = Console.ReadLine()!;
 string[] subs = userEnter.Split();
 Console.WriteLine($"\n в строке слова, у которых меньше 3 символов: ");
 for (int i = 0; i < subs.Length; i++)
+{
+    string temp = subs[i];
+        if (temp.Length <= 3)
+    {
+        Console.Write(temp + " ");
+    }
+}
